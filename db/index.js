e1d6329fe5029b3b30adbda36cc2a3de3348703a
Asuka Matoba <asukamatoba511@gmail.com>
2024-01-29 19:43:10 +0800

    add env and dockercompose and db

---
+++ db/index.js
@@ -0,0 +1,28 @@
+"use strict";
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
+
+const connectMysql = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log(`Successful connection to MySQL ${process.env.DB_NAME}`);
+  } catch (error) {
+    console.error("Unable to connect to MySQL :", error);
+    process.exit(1); //to stop Node.js process when error occures.  this code makes the process end with errorcode 1(it means the program finishes with error)
+  }
+};
+
+connectMysql();
+
+module.exports = {
+  Sequelize: sequelize,
+};
